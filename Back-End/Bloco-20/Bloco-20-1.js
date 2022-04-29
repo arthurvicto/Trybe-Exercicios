77d6996af152550bfcd0d48d873a2ab11e4da714
Arthur Victoriano <arthurvicto@hotmail.com>
2022-04-29 16:13:55 -0300

    Bloco 20.1

---
+++ Back-End/Bloco-20/Bloco-20-1.js
@@ -0,0 +1,19 @@
+1. Entre no banco de dados mysql 
+
+mysql -u root -p
+
+2. Visualize todas as tabelas desse banco
+
+USE nome_do_banco_de_dados_aqui;
+SHOW TABLES;
+
+3. Visualize a estrutura de pelo menos 10 tabelas
+ diferentes e tente entender o tipo de estrutura que costuma 
+ ser utilizada
+
+ DESCRIBE nome_da_tabela;
+
+ 4. Crie um novo banco de dados com o seu nome e depois entre nele
+
+ CREATE DATABASE seu_nome_aqui;
+USE seu_nome_aqui;
